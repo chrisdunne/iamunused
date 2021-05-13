ce984680d92f2074d05c5ea1f1874c29906bae11
Christopher Dunne <contact@chrisdunne.net>
2021-05-13 22:24:18 +0100

    introduce get_roles and get_unused_role_permissions with basic tests

--- iamunused/reader.py
+++ iamunused/reader.py
@@ -1,4 +1,33 @@
 # reader.py
+import boto3
 
-def get_unused():
-    print("Coming soon...")
+class Reader(object):
+    def __init__(self, type, days):
+        self.type = type
+        self.days = days
+        self.iam = boto3.client('iam')
+
+    def get_roles(self):
+        response = self.iam.list_roles()
+        return response['Roles']
+
+    def get_unused_role_permissions(self, role):
+        unused = []
+        
+        job_id = self.iam.generate_service_last_accessed_details(
+            Arn = role['Arn'],
+            Granularity = 'SERVICE_LEVEL'
+        )
+
+        response = self.iam.get_service_last_accessed_details(
+            JobId = job_id['JobId']
+        )
+
+        for service in response['ServicesLastAccessed']:
+            if service['LastAuthenticated'] is not None:
+                unused.append(service['ServiceName'])
+        
+        return unused
+
+    def __exit__(self):
+        self.iam.close()
